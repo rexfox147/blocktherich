e2f5a95c74b3cccfd3e01741b3ddb58b08b006dd
rexfox147 <janmair@gmx.de>
2025-01-12 01:10:16 +0100

    added default values

--- popup/options.js
+++ popup/options.js
@@ -2,9 +2,30 @@ const entriesContainer = document.getElementById("entries");
 const addEntryButton = document.getElementById("add-entry");
 const saveButton = document.getElementById("save");
 
+const defaultBlackList = [
+    { name: "ballmer", list: ["steve ballmer", "steven anthony ballmer"], toggle: false },
+    { name: "bezos", list: ["bezos", "jeff bezos"], toggle: false },
+    { name: "bloomberg", list: ["michael bloomberg", "michael rubens bloomberg"], toggle: false },
+    { name: "brin", list: ["sergey brin", "sergey mikhailovich brin"], toggle: false },
+    { name: "buffett", list: ["buffett", "warren buffett", "warren edward buffett"], toggle: false },
+    { name: "ellison", list: ["larry ellison", "lawrence joseph ellison"], toggle: false },
+    { name: "gates", list: ["bill gates", "william henry gates iii"], toggle: false },
+    { name: "kanye", list: ["kanye", "kanye west", "ye west", "kanye omari west"], toggle: false },
+    { name: "musk", list: ["musk", "elon musk", "elon reeve musk"], toggle: false },
+    { name: "page", list: ["larry page", "lawrence edward page"], toggle: false },
+    { name: "trump", list: ["trump", "donald trump", "donald j. trump", "donald john trump"], toggle: false },
+    { name: "zuck", list: ["zuck", "zuckerberg", "mark zuckerberg", "mark elliot zuckerberg"], toggle: false }
+];
+
+
 // Load entries from storage
 async function loadEntries() {
-    const { configEntries = [] } = await browser.storage.local.get("configEntries");
+    let {configEntries = []} = await browser.storage.local.get("configEntries");
+
+    if(!configEntries || configEntries.length === 0 ) {
+        configEntries = defaultBlackList
+    }
+
     entriesContainer.innerHTML = "";
     configEntries.forEach((entry, index) => addEntry(entry, index));
 }
