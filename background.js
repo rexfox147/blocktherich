c371f0190a0fa0284816fc3f70684bd51c632e36
rexfox147 <janmair@gmx.de>
2025-01-13 01:30:58 +0100

    properly setting the default

--- background.js
+++ background.js
@@ -1,3 +1,19 @@
+const defaultBlackList = [
+    { name: "ballmer", list: ["steve ballmer", "steven anthony ballmer"], toggle: false },
+    { name: "bezos", list: ["bezos", "jeff bezos"], toggle: false },
+    { name: "bloomberg", list: ["michael bloomberg", "michael rubens bloomberg"], toggle: false },
+    { name: "brin", list: ["sergey brin", "sergey mikhailovich brin"], toggle: false },
+    { name: "buffett", list: ["buffett", "warren buffett", "warren edward buffett"], toggle: false },
+    { name: "ellison", list: ["larry ellison", "lawrence joseph ellison"], toggle: false },
+    { name: "gates", list: ["bill gates", "william henry gates iii"], toggle: false },
+    { name: "kanye", list: ["kanye", "kanye west", "ye west", "kanye omari west"], toggle: false },
+    { name: "musk", list: ["musk", "elon musk", "elon reeve musk"], toggle: false },
+    { name: "page", list: ["larry page", "lawrence edward page"], toggle: false },
+    { name: "trump", list: ["trump", "donald trump", "donald j. trump", "donald john trump"], toggle: false },
+    { name: "zuck", list: ["zuck", "zuckerberg", "mark zuckerberg", "mark elliot zuckerberg"], toggle: false }
+];
+
+
 function handleClick() {
     checkExtensionStatus();
 }
@@ -26,8 +42,23 @@ function saveStatus(value) {
     browser.storage.local.set({ "status": value });
 }
 
+async function ensureDefault(){
+    let {configEntries = []} = await browser.storage.local.get("configEntries");
+
+    if(!configEntries || configEntries.length === 0 ) {
+        configEntries = defaultBlackList
+        await browser.storage.local.set({ configEntries: configEntries });
+    }
+}
+
 browser.runtime.onInstalled.addListener(() => { saveStatus(true); });
 
 browser.runtime.onStartup.addListener(() => { saveStatus(true); });
 
 browser.browserAction.onClicked.addListener(handleClick);
+
+browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+        ensureDefault();
+    }
+});
